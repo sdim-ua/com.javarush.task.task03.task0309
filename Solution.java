8545495579dc3eb0186aa5b15ab1269380e9e935
sdim-ua <ingersol@yandex.ua>
2019-01-04 11:41:18 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,16 @@
+/* 
+Сумма 5 чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        
+        int sum = 0;
+        for (int i = 0; i < 5; i++){
+           sum = sum + i;
+      System.out.println(sum);
+           
+        }
+    }
+}
